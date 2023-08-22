c254b74b188d0ba05c10628c093f0b042c4d7d17
Roman <89307076692roman@gmail.com>
2023-08-22 20:49:42 +0300

    icon

--- app/src/main/java/com/example/school121/MainActivity.java
+++ app/src/main/java/com/example/school121/MainActivity.java
@@ -4,37 +4,32 @@ import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         WebView webView = findViewById(R.id.webView);
         webView.loadUrl("https://sch121.edusite.ru/");
-
-
         WebViewClient webViewClient = new WebViewClient() {
-
             @SuppressWarnings("deprecation")
             @Override
-
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-
                 view.loadUrl(url);
-
                 return true;
-
             }
-
 
             @TargetApi(Build.VERSION_CODES.N)
             @Override
@@ -46,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
                 return true;
 
             }
-
         };
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(webViewClient);
         webView.getSettings().setLoadWithOverviewMode(true);
@@ -55,11 +50,30 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setSupportZoom(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
-        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        webView.setScrollbarFadingEnabled(false);
-        webView.setInitialScale(200);
-        webView.setVerticalScrollBarEnabled(false);
-        webView.setHorizontalScrollBarEnabled(false);
+        webView.setInitialScale(100);
+
+
+        webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(true);
+        webView.setScrollBarFadeDuration(2000);
+
 
     }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack())
+            webView.goBack();// if there is previous page open it
+        else showInfoAlert();
+    }
+
+
+    private void showInfoAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Большая подсказка").setMessage("Вы хотите закрыть приложение?").setCancelable(true).setPositiveButton("Да", (dialog, which) -> finish()).setNegativeButton("Нет", (dialogInterface, which) -> dialogInterface.cancel());
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
 }
